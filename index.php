a02a7a1c7264dc001284dc8b53520d0fef09f336
venturion <felipeventurino@hotmail.com>
2014-02-15 22:11:51 -0200

    commit #1 #2

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-	echo "xxx";   
-?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Titulo do Site</title>
+</head>
+<body>
+	 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta est consequatur aut dicta obcaecati omnis saepe illum non numquam aliquid maiores voluptas rerum ad possimus reiciendis quaerat quibusdam officia impedit.
+</body>
+</html>
